b2244e4dfb6634d812a2c00ed3e8d8c8a656df79
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2019-12-02 09:44:07 -0600

    Day 2 solutions

--- day02/puzzle1.py
+++ day02/puzzle1.py
@@ -6,7 +6,63 @@ import click
 @click.argument("input_file", type=click.File("r"))
 def main(input_file):
     """Put your puzzle execution code here"""
-    print(input_file)
+    # Convert the comma-delimited string of numbers into a list of ints
+    register = list(
+        map(lambda op: int(op), input_file.read().strip().split(","))
+    )
+
+    # Modify the second and third codes for the final result
+    register[1] = 12
+    register[2] = 2
+
+    # We will start reading the opcodes at position 0
+    pointer = 0
+
+    # Loop infinitely until we reach the termination instruction
+    while True:
+        # Get the code at the current read position
+        code = register[pointer]
+
+        # Code 99 means immediate termination
+        if code == 99:
+            break
+
+        # Code 1 is addition
+        elif code == 1:
+            # Get register addresses
+            addendAPointer = register[pointer + 1]
+            addendBPointer = register[pointer + 2]
+            sumPointer = register[pointer + 3]
+
+            # Perform the addition
+            register[sumPointer] = (
+                register[addendAPointer] + register[addendBPointer]
+            )
+
+            # Advance the code position by 4
+            pointer += 4
+
+        # Code 2 is multiplication
+        elif code == 2:
+            # Get register addresses
+            factorAPointer = register[pointer + 1]
+            factorBPointer = register[pointer + 2]
+            productPointer = register[pointer + 3]
+
+            # Perform the addition
+            register[productPointer] = (
+                register[factorAPointer] * register[factorBPointer]
+            )
+
+            # Advance the code position by 4
+            pointer += 4
+
+        # Unknown opcode means there was an error
+        else:
+            raise RuntimeError(f"Unknown opcode {code} at position {pointer}")
+
+    # Print the the result remaining in postion 0
+    print("RESULT:", register[0])
 
 
 # Execute cli function on main
--- day02/puzzle2.py
+++ day02/puzzle2.py
@@ -1,3 +1,6 @@
+# stdlib imports
+import copy
+
 # vendor imports
 import click
 
@@ -6,7 +9,84 @@ import click
 @click.argument("input_file", type=click.File("r"))
 def main(input_file):
     """Put your puzzle execution code here"""
-    print(input_file)
+    # Convert the comma-delimited string of numbers into a list of ints
+    masterRegister = list(
+        map(lambda op: int(op), input_file.read().strip().split(","))
+    )
+
+    def execute(noun, verb):
+        # Create a local copy of the register for this execution
+        register = copy.deepcopy(masterRegister)
+
+        # Inject the noun and verb
+        register[1] = noun
+        register[2] = verb
+
+        # We will start reading the opcodes at position 0
+        pointer = 0
+
+        # Loop infinitely until we reach the termination instruction
+        while True:
+            # Get the code at the current read position
+            code = register[pointer]
+
+            # Code 99 means immediate termination
+            if code == 99:
+                break
+
+            # Code 1 is addition
+            elif code == 1:
+                # Get register addresses
+                addendAPointer = register[pointer + 1]
+                addendBPointer = register[pointer + 2]
+                sumPointer = register[pointer + 3]
+
+                # Perform the addition
+                register[sumPointer] = (
+                    register[addendAPointer] + register[addendBPointer]
+                )
+
+                # Advance the code position by 4
+                pointer += 4
+
+            # Code 2 is multiplication
+            elif code == 2:
+                # Get register addresses
+                factorAPointer = register[pointer + 1]
+                factorBPointer = register[pointer + 2]
+                productPointer = register[pointer + 3]
+
+                # Perform the addition
+                register[productPointer] = (
+                    register[factorAPointer] * register[factorBPointer]
+                )
+
+                # Advance the code position by 4
+                pointer += 4
+
+            # Unknown opcode means there was an error
+            else:
+                raise RuntimeError(
+                    f"Unknown opcode {code} at position {pointer}"
+                )
+
+        # Return the result
+        return register[0]
+
+    # Iterate through all the possible combinations until the target is found
+    target = 19690720
+    found = None
+    for noun in range(100):
+        for verb in range(100):
+            result = execute(noun, verb)
+            if result == target:
+                found = (noun, verb)
+                break
+        if found:
+            break
+
+    # Calculate the final result
+    print("RESULT:", 100 * found[0] + found[1])
 
 
 # Execute cli function on main
