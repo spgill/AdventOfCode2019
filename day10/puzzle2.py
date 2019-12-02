6f921423f3ce7e2fe6e88826e0a1ca8d972aa001
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2019-12-02 09:47:08 -0600

    Applied template to the rest of the days

--- day10/puzzle2.py
+++ day10/puzzle2.py
@@ -1,1 +1,14 @@
-print('I\'m empty!')
+# vendor imports
+import click
+
+
+@click.command()
+@click.argument("input_file", type=click.File("r"))
+def main(input_file):
+    """Put your puzzle execution code here"""
+    print(input_file)
+
+
+# Execute cli function on main
+if __name__ == "__main__":
+    main()
