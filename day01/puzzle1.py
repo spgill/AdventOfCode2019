1283c0e805845dc214b4d0772d99f65322f63f98
Samuel Gillispie <samuel@spgill.me>
2019-12-02 09:00:55 -0600

    Day 01 solutions

--- day01/puzzle1.py
+++ day01/puzzle1.py
@@ -1,1 +1,24 @@
-print('I\'m empty!')
+# stdlib imports
+import math
+
+# vendor imports
+import click
+
+
+@click.command()
+@click.argument("input_file", type=click.File("r"))
+def main(input_file):
+    """Put your puzzle execution code here"""
+    modules = input_file.read().strip().splitlines()
+    fuelRequirements = []
+
+    for module in modules:
+        mass = int(module)
+        fuelRequirements.append(math.floor(mass / 3) - 2)
+
+    print("RESULT:", sum(fuelRequirements))
+
+
+# Execute cli function on main
+if __name__ == "__main__":
+    main()
--- day01/puzzle2.py
+++ day01/puzzle2.py
@@ -1,1 +1,36 @@
-print('I\'m empty!')
+# stdlib imports
+import math
+
+# vendor imports
+import click
+
+
+@click.command()
+@click.argument("input_file", type=click.File("r"))
+def main(input_file):
+    """Put your puzzle execution code here"""
+    modules = input_file.read().strip().splitlines()
+    fuelRequirements = []
+
+    for module in modules:
+        mass = int(module)
+
+        moduleFuelReq = math.floor(mass / 3) - 2
+        fuelRequirements.append(moduleFuelReq)
+        print("MODULE", moduleFuelReq)
+
+        fuelReqCounter = moduleFuelReq
+        while True:
+            req = math.floor(fuelReqCounter / 3) - 2
+            print("   ", req)
+            if req <= 0:
+                break
+            fuelRequirements.append(req)
+            fuelReqCounter = req
+
+    print("RESULT:", sum(fuelRequirements))
+
+
+# Execute cli function on main
+if __name__ == "__main__":
+    main()
