26bdd106cc01414872efd1efdd5da8f2a0406c3f
Samuel Gillispie <samuel@spgill.me>
2019-12-08 12:54:38 -0600

    Day 8 solutions

--- day08/puzzle1.py
+++ day08/puzzle1.py
@@ -4,9 +4,29 @@ import click
 
 @click.command()
 @click.argument("input_file", type=click.File("r"))
-def main(input_file):
+@click.argument("image_width", type=int)
+@click.argument("image_height", type=int)
+def main(input_file, image_width, image_height):
     """Put your puzzle execution code here"""
-    print(input_file)
+    # Load the image data string
+    imageData = input_file.read().strip()
+
+    # Split the string into layers
+    layerSize = image_width * image_height
+    layerData = [
+        imageData[layerSize * i : layerSize * (i + 1)]
+        for i in range(len(imageData) // layerSize)
+    ]
+
+    # Count the digits in the layers
+    layerCounts = [
+        (layer.count("0"), layer.count("1") * layer.count("2"))
+        for layer in layerData
+    ]
+
+    # The result is the layer with the least 0's, and the count of that
+    # layer's 1's and 2's multiplied
+    print("RESULT:", min(layerCounts, key=lambda l: l[0])[1])
 
 
 # Execute cli function on main
--- day08/puzzle2.py
+++ day08/puzzle2.py
@@ -1,12 +1,47 @@
+# stdlib imports
+import sys
+
 # vendor imports
 import click
 
 
+blocks = {"0": " ", "1": "█"}
+
+
 @click.command()
 @click.argument("input_file", type=click.File("r"))
-def main(input_file):
+@click.argument("image_width", type=int)
+@click.argument("image_height", type=int)
+def main(input_file, image_width, image_height):
     """Put your puzzle execution code here"""
-    print(input_file)
+    # Load the image data string
+    imageData = input_file.read().strip()
+
+    # Split the string into layers
+    layerSize = image_width * image_height
+    layerData = [
+        imageData[layerSize * i : layerSize * (i + 1)]
+        for i in range(len(imageData) // layerSize)
+    ]
+
+    # Iterate through the size of the image and multiplex the layers
+    # into the final image
+    for y in range(image_height):
+        for x in range(image_width):
+            address = image_width * y + x
+            pixel = None
+
+            # Iterate through each layer for the pixel value
+            for layer in layerData:
+                pixel = layer[address]
+                if pixel != "2":
+                    break
+
+            # Print the pixel to the console
+            sys.stdout.write(blocks[pixel])
+        sys.stdout.write("\n")
+
+    print()
 
 
 # Execute cli function on main
