965c7e503515a4f3611b928df169920e7d5dc886
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2019-12-12 13:33:32 -0600

    Day 12 solutions

--- day12/puzzle1.py
+++ day12/puzzle1.py
@@ -1,3 +1,9 @@
+# stdlib imports
+import itertools
+import math
+import re
+import types
+
 # vendor imports
 import click
 
@@ -5,8 +11,54 @@ import click
 @click.command()
 @click.argument("input_file", type=click.File("r"))
 def main(input_file):
-    """Put your puzzle execution code here"""
-    print(input_file)
+    # Read the input file and parse out the vectors
+    moons = [
+        types.SimpleNamespace(
+            pos=(int(match[0]), int(match[1]), int(match[2])), vel=(0, 0, 0),
+        )
+        for match in re.findall(
+            r"^\D+?(-?\d+)\D+?(-?\d+)\D+?(-?\d+)",
+            input_file.read().strip(),
+            re.MULTILINE,
+        )
+    ]
+
+    # Iterate through 1000 steps of simulation
+    for n in range(1000):
+        # We first update velocities by iterating through all possible pairs
+        for moonA, moonB in itertools.combinations(moons, 2):
+            moonA.vel = tuple(
+                vel
+                + (
+                    0
+                    if moonA.pos[i] == moonB.pos[i]
+                    else math.copysign(1, moonB.pos[i] - moonA.pos[i])
+                )
+                for i, vel in enumerate(moonA.vel)
+            )
+            moonB.vel = tuple(
+                vel
+                + (
+                    0
+                    if moonB.pos[i] == moonA.pos[i]
+                    else math.copysign(1, moonA.pos[i] - moonB.pos[i])
+                )
+                for i, vel in enumerate(moonB.vel)
+            )
+
+        # Now increment the positions by the velocities
+        for moon in moons:
+            moon.pos = tuple(moon.pos[i] + moon.vel[i] for i in range(3))
+
+    # Calculate the total energy in the system
+    energy = 0
+    for moon in moons:
+        energy += sum([abs(n) for n in moon.pos]) * sum(
+            [abs(n) for n in moon.vel]
+        )
+
+    # Print the result (as an int)
+    print("RESULT:", int(energy))
 
 
 # Execute cli function on main
--- day12/puzzle2.py
+++ day12/puzzle2.py
@@ -1,12 +1,86 @@
+# stdlib imports
+import copy
+import itertools
+import math
+import re
+import types
+
 # vendor imports
 import click
+
+
+# Convenience function for calculating Least Common Multiple
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
 
 
 @click.command()
 @click.argument("input_file", type=click.File("r"))
 def main(input_file):
-    """Put your puzzle execution code here"""
-    print(input_file)
+    # Print warning
+    print("WARNING: This may take a while")
+
+    # Read the input file and parse out the vectors
+    moons = [
+        types.SimpleNamespace(
+            pos=(int(match[0]), int(match[1]), int(match[2])), vel=(0, 0, 0),
+        )
+        for match in re.findall(
+            r"^\D+?(-?\d+)\D+?(-?\d+)\D+?(-?\d+)",
+            input_file.read().strip(),
+            re.MULTILINE,
+        )
+    ]
+
+    # Make a copy of the initial state for comparison
+    initialMoons = copy.deepcopy(moons)
+
+    # Iterate indefinitely to find the rotational periods for each axis
+    periods = [None, None, None]
+    n = 1
+    while True:
+        # We first update velocities by iterating through all possible pairs
+        for moonA, moonB in itertools.combinations(moons, 2):
+            moonA.vel = tuple(
+                vel
+                + (
+                    0
+                    if moonA.pos[i] == moonB.pos[i]
+                    else math.copysign(1, moonB.pos[i] - moonA.pos[i])
+                )
+                for i, vel in enumerate(moonA.vel)
+            )
+            moonB.vel = tuple(
+                vel
+                + (
+                    0
+                    if moonB.pos[i] == moonA.pos[i]
+                    else math.copysign(1, moonA.pos[i] - moonB.pos[i])
+                )
+                for i, vel in enumerate(moonB.vel)
+            )
+
+        # Now increment the positions by the velocities
+        for moon in moons:
+            moon.pos = tuple(moon.pos[i] + moon.vel[i] for i in range(3))
+
+        # Increment loop counter
+        n += 1
+
+        # Check for repeating axises
+        for axis in range(3):
+            matches = 0
+            for i, moon in enumerate(moons):
+                if moon.pos[axis] == initialMoons[i].pos[axis]:
+                    matches += 1
+            if matches == len(moons) and periods[axis] is None:
+                periods[axis] = n
+
+        # If all periods have been found, break the loop
+        if None not in periods:
+            break
+
+    print("RESULT:", lcm(lcm(periods[0], periods[1]), periods[2]))
 
 
 # Execute cli function on main
